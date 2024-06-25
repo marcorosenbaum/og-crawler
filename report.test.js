730d1ccdd451da6d6aa91be4a395685193f42198
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-06-25 18:45:11 +0200

    create report and print to console

---
+++ report.test.js
@@ -0,0 +1,34 @@
+const { sortPages } = require("./report");
+const { expect, test } = require("@jest/globals");
+
+test("sortPages, 2pages", () => {
+  const input = {
+    "https://coding-bootcamps.eu": 3,
+    "https://coding-bootcamps.eu/about": 7,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://coding-bootcamps.eu/about", 7],
+    ["https://coding-bootcamps.eu", 3],
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("sortPages 5 pages", () => {
+  const input = {
+    "https://coding-bootcamps.eu": 3,
+    "https://coding-bootcamps.eu/path4": 17,
+    "https://coding-bootcamps.eu/path3": 11,
+    "https://coding-bootcamps.eu/path2": 1,
+    "https://coding-bootcamps.eu/path1": 5,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://coding-bootcamps.eu/path4", 17],
+    ["https://coding-bootcamps.eu/path3", 11],
+    ["https://coding-bootcamps.eu/path1", 5],
+    ["https://coding-bootcamps.eu", 3],
+    ["https://coding-bootcamps.eu/path2", 1],
+  ];
+  expect(actual).toEqual(expected);
+});
