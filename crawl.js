63922b6a04e820e51f0bffc419ad27ad392e8b1f
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-06-25 17:35:40 +0200

    crawl page recursiv working

--- crawl.js
+++ crawl.js
@@ -1,23 +1,46 @@
 const normalizeUrl = require("@esm2cjs/normalize-url").default;
 const { JSDOM } = require("jsdom");
 
-const crawlPage = async (currentURL) => {
+const crawlPage = async (baseURL, currentURL, pages) => {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`actively crawling ${currentURL}`);
+
   try {
     const response = await fetch(currentURL);
     if (response.status > 399) {
-      console.log(`Error fetching ${currentURL}: ${response.status}`);
-      return;
+      console.error(`- - Error fetching ${currentURL}: ${response.status}`);
+      return pages;
     }
     const contentType = response.headers.get("content-type");
     if (!contentType.includes("text/html")) {
-      console.log(`None html respone for ${currentURL}`);
+      console.error(`- - None html respone for ${currentURL}`);
+      return pages;
     }
 
-    console.log(await response.text());
+    const htmlBody = await response.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
-    console.log(`Error fetching ${currentURL}: ${error.message}`);
+    console.error(`- - Error fetching ${currentURL}: ${error.message}`);
   }
+  return pages;
 };
 
 const normalizeURL = (url) => {
@@ -36,14 +59,16 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
         const urlObj = new URL(`${baseURL}${linkElement.href}`);
         urls.push(urlObj.href);
       } catch (error) {
-        console.log(`Error with relative url: ${error.message}`);
+        console.error(`- - Error with relative url: ${error.message}`);
       }
     } else {
       try {
         const urlObj = new URL(linkElement.href);
         urls.push(urlObj.href);
       } catch (error) {
-        console.log(`Error with absolute url: ${error.message}`);
+        console.error(
+          `- - Error with absolute url: ${linkElement.href}: ${error.message}`
+        );
       }
     }
   }
